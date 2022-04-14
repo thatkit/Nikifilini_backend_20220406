f92df847d929be10cd1b67f00d50675063469fb1
Nikita <emper137137@gmail.com>
2022-04-14 16:23:05 +1000

    findOrder with by=id filter

--- src/retail_api/retail.service.ts
+++ src/retail_api/retail.service.ts
@@ -46,8 +46,7 @@ export class RetailService {
   }
 
   async findOrder(id: string): Promise<Order | null> {
-    const resp = await this.axios.get('/orders/' + id + '?site=demo-magazin')
-    // # modify to {{retailUrl}}/api/v5/orders/{{invalidId}}?site=demo-magazin&by=id
+    const resp = await this.axios.get('/orders/' + id + '?site=demo-magazin&by=id')
 
     if (!resp.data) throw new Error('RETAIL CRM ERROR')
 
